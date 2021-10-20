99688ec2a7c903730557c592f1338a867cf6aa2a
= <=>
2021-10-20 11:51:34 +0400

    styles: add new design of blog app

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+{{-- @extends('layout')
 
 
 
@@ -28,5 +28,31 @@
 
     @endforeach
 
-@endsection
+@endsection --}}
+
+
+<x-layout>
+
+    @include('posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+
+</x-layout>
+
+
+
+
 
