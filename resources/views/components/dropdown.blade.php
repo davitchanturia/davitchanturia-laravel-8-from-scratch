d3198748208863044fa616cb16f643dfa6a7de46
= <=>
2021-10-22 11:57:20 +0400

    style: minimal changes in marckup nothing important

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -10,7 +10,7 @@
     </div>
     
     {{-- links --}}
-    <div x-show=" show " class="py-2 absolute bg-gray-100 w-full rounded-xl mt-2">
+    <div x-show=" show " class="py-2 absolute bg-gray-100 w-full rounded-xl mt-2 overflow-auto max-h-52">
       
         {{ $slot }}
         
