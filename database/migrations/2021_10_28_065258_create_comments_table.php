9bebb46a51482b4465576704593a7b2d1066b588
dato chanturia <datochanturia@redberry.ge>
2021-10-28 16:29:18 +0400

    sadasd

--- database/migrations/2021_10_28_065258_create_comments_table.php
+++ database/migrations/2021_10_28_065258_create_comments_table.php
@@ -30,7 +30,7 @@ class CreateCommentsTable extends Migration
             // $table->foreign('user_id')->references('id')->ong('users')->cascadeOnDelete();
         
         });
-    }
+    }sadasd
 
     /**
      * Reverse the migrations.
